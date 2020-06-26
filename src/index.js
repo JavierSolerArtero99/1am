bea74738e31d87709341332a33dfc58a05f514c3
javier_soler <javier.soler.jsa@gmail.com>
2020-06-26 14:04:12 +0200

    loading a falta de el correcto margen

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MainPage from './screens/MainPage'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MainPage />,
   document.getElementById('root')
 );
 
